adfe945a3c85e7df8e0f4cf0cb283ac174df58e7
Jamie Dalan Cox <74336097+dalancox@users.noreply.github.com>
2022-07-13 21:24:28 -0600

    added style changes and more articles

--- src/pages/wall-diffuser/index.js
+++ src/pages/wall-diffuser/index.js
@@ -3,6 +3,9 @@ import Layout from "../../components/layout"
 import WallDiffuserHeader from "../../components/headers/wallDiffuserHeader"
 import { Link } from "gatsby"
 
+
+import * as styles from "../styles/catagory.module.css"
+
 export default function Index() {
   return (
   
@@ -15,29 +18,76 @@ export default function Index() {
           padding: `var(--size-gutter)`,
         }}
       >
-        <div style={{display: `flex`, justifyContent: `center`, fontSize: `var(--font-lx)`}}>
+        <div className={styles.title}>
           <h1>Wall Diffuser</h1>
         </div>
         <div>
-          <h1 style={{marginBottom: `1rem`}}>Diffuser Setup and Features</h1>
+          <h1 className={styles.subTitle}>Diffuser Setup and Features</h1>
           <p>Set up and navigate your diffuser and its features</p>
-          <div style={{borderBottom: `1px solid #ddd`}}></div>
+          <div className={styles.breakLine}></div>
           <h3>Diffuser Setup</h3>
-          <div style={{display: `flex`, justifyContent: `space-between`, flexWrap: `wrap`, gap: `1rem`}}>
+          <div className={styles.articles}>
             <Link to="/article/setting-up-your-pura-device">
-              <div style={{backgroundColor: `#ddd`, width: `325px`, padding: `1rem`, borderRadius: `5px`}} >
-                <h4 style={{margin: `0`}}>Set Up Your Pura Diffuser</h4>
-                <p style={{margin: `0`, fontSize: `12px`}}>Learn how to setup your Pura diffuser!</p>
-              </div>
-            </Link>
-            <Link to="/article/diffuser-offline">
-              <div style={{backgroundColor: `#ddd`, width: `325px`, padding: `1rem`, borderRadius: `5px`}} >
-                <h4 style={{margin: `0`}}>Diffuser Offline</h4>
-                <p style={{margin: `0`, fontSize: `12px`}}>Learn how to reconnect your Pura diffuser!</p>
+              <div className={styles.subArticles}>
+                <h4>Set Up Your Pura Diffuser</h4>
+                <p>Learn how to setup your Pura diffuser!</p>
               </div>
             </Link>
           </div>
-          
+          <h3>Diffuser Feature</h3>
+          <div className={styles.articles}>
+            <Link to="/">
+              <div className={styles.subArticles}>
+                <h4>Priority of Features</h4>
+                <p>Learn what features take priority!</p>
+              </div>
+            </Link>
+            <Link to="/">
+              <div className={styles.subArticles}>
+                <h4>Schedules</h4>
+                <p>Set up and manage schedules.</p>
+              </div>
+            </Link>
+            <Link to="/">
+              <div className={styles.subArticles}>
+                <h4>Replace a low fragrance</h4>
+                <p>How to replace a low fragrance.</p>
+              </div>
+            </Link>
+            <Link to="/">
+              <div className={styles.subArticles}>
+                <h4>Set Up Your Pura Diffuser</h4>
+                <p>Learn how to setup your Pura diffuser!</p>
+              </div>
+            </Link>
+          </div>
+          <h3>Diffuser Troubleshooting</h3>
+          <div className={styles.articles}>
+            <Link to="/article/diffuser-offline">
+              <div className={styles.subArticles}>
+                <h4>Diffuser Offline</h4>
+                <p>Learn how to reconnect your Pura diffuser!</p>
+              </div>
+            </Link>
+            <Link to="/">
+              <div className={styles.subArticles}>
+                <h4>WiFi Network Requirements</h4>
+                <p>Learn what network you need.</p>
+              </div>
+            </Link>
+            <Link to="/">
+              <div className={styles.subArticles}>
+                <h4>Weak Smell</h4>
+                <p>Learn why you can't smell your Pura diffuser!</p>
+              </div>
+            </Link>
+            <Link to="/">
+              <div className={styles.subArticles}>
+                <h4>Bluetooth</h4>
+                <p>Learn how to connect the diffuser to Bluetooth.</p>
+              </div>
+            </Link>
+          </div>
         </div>
         <Link to="/">Go home</Link>
       </div>
